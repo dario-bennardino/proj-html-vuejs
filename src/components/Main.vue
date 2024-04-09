08037b729abe82a390d4dbca39f3c7fa90cf9c2a
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-04-09 21:30:00 +0200

    [add] preparate ultime section

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -42,12 +42,12 @@ import CardGallery from './CardGallery.vue';
         <div class="container">
             <h1>Working <span>process</span> </h1>
             <div class="working-process-gallery">
-                <!-- <img src="/public/assets/svg/svg-3.svg" alt=""> -->
+                <img src="/public/assets/svg/svg-3.svg" alt="">
                 <div class="row">
-                    <div class="col-4">
+                    <div class="col-3">
                         <div class="card-img">
                             <div class="card-img-fix d-flex justify-content-center align-items-center ">
-                                <i class="fa-solid fa-lightbulb"></i>
+                                <i class="fa-solid fa-lightbulb first-icon"></i>
                             </div>
                         </div>
                         <div class="card-text text-center d-flex align-item-center flex-column justify-content-center">
@@ -56,35 +56,58 @@ import CardGallery from './CardGallery.vue';
                         </div>
                     </div>
 
-                    <div class="col-4">
+                    <div class="col-3">
                         <div class="card-img">
                             <div class="card-img-fix d-flex justify-content-center align-items-center ">
-                                <i class="fa-solid fa-cloud"></i>
+                                <i class="fa-regular fa-comment second-icon"></i>
                             </div>
                         </div>
                         <div class="card-text text-center d-flex align-item-center flex-column justify-content-center">
-                            <h5>First theres an idea</h5>
+                            <h5>Then we talk about</h5>
                             <h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque aperiam quas tempore sequi.</h6>
                         </div>
                     </div>
 
-                    <div class="col-4">
+                    <div class="col-3">
                         <div class="card-img">
                             <div class="card-img-fix d-flex justify-content-center align-items-center ">
-                                <i class="fa-solid fa-pen"></i>
+                                <i class="fa-solid fa-cloud third-icon"></i>
                             </div>
                         </div>
                         <div class="card-text text-center d-flex align-item-center flex-column justify-content-center">
-                            <h5>First theres an idea</h5>
+                            <h5>And we thing about</h5>
                             <h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque aperiam quas tempore sequi.</h6>
                         </div>
                     </div>
-                    <!-- <div class="col-4">colonna</div>
-                    <div class="col-4">colonna</div> -->
+
+                    <div class="col-3">
+                        <div class="card-img">
+                            <div class="card-img-fix d-flex justify-content-center align-items-center ">
+                                <i class="fa-solid fa-pen last-icon"></i>
+                            </div>
+                        </div>
+                        <div class="card-text text-center d-flex align-item-center flex-column justify-content-center">
+                            <h5>So we draw along</h5>
+                            <h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque aperiam quas tempore sequi.</h6>
+                        </div>
+                    </div>
+                  
                 </div>
             </div>
         </div>
 
+    </section>
+
+    <section class="work-with-us">
+        <div class="container">
+            Wanto to work with us?
+        </div>
+    </section>
+
+    <section class="events">
+        <div class="container">
+            last section
+        </div>
     </section>
 </template>
 
@@ -120,12 +143,12 @@ import CardGallery from './CardGallery.vue';
             .container{
                 width: 1300px;
                 height: 80%;
-                border: 1px solid black;
+                // border: 1px solid black;
+                margin-bottom: 50px;
 
                 h1{
                     color: black;
                     text-align: center;
-                    // margin-top: 50px;
                     font-size: 4rem;
 
                     span{
@@ -136,31 +159,32 @@ import CardGallery from './CardGallery.vue';
                 }
 
                 .working-process-gallery{
-                    // border: 1px solid black;
+                    position: relative;
                     height: 300px;
-                    // text-align: center;
                     display: flex;
                     align-items: center;
                     justify-content: center;
-                    margin: 50px;
+                    margin-top: 100px;
                     img{
-                        width: 100%;
+                        width: 90%;
+                        margin-bottom: 200px;
                     }
 
                     .row{
                         height: 100%;
                         width: 100%;
+                        position: absolute;
+                        // top: 90px;
 
-                        .col-4{
+
+                        .col-3{
                             height: 100%;
-                            // background-color: aqua;
-                            border: 1px solid black;
+                            // border: 1px solid black;
                             
 
                             .card-img{
                                 height: 50%;
                                 width: 100%;
-                                // border: 1px solid black;
                                 display: flex;
                                 align-items: center;
                                 justify-content: center;
@@ -173,8 +197,24 @@ import CardGallery from './CardGallery.vue';
                                     background-color: $bg-color-6;
 
                                     i{
-                                        color: $bg-color-8;
+                                        // color: $bg-color-8;
                                         font-size: 2rem;
+                                    }
+
+                                    .first-icon{
+                                        color: $bg-color-8;
+                                    }
+
+                                    .second-icon{
+                                        color: $text-color-3;
+                                    }
+
+                                    .last-icon{
+                                        color: $text-color-7;
+                                    }
+
+                                    .third-icon{
+                                        color: $text-color-10;
                                     }
                                 }
                             }
@@ -182,7 +222,6 @@ import CardGallery from './CardGallery.vue';
                             .card-text{
                                 height: 50%;
                                 width: 100%;
-                                // border: 1px solid black;
 
                                 h5{
                                     color: $text-color-11;
@@ -190,6 +229,7 @@ import CardGallery from './CardGallery.vue';
 
                                 h6{
                                     color: $text-color-5;
+                                    padding-top: 5px;
                                 }
                             }
                         }
@@ -197,5 +237,28 @@ import CardGallery from './CardGallery.vue';
                 }
             }
         }
+
+    .work-with-us{
+        width: 100vw;
+        height: 525px;
+        background-color: $bg-color-6;
+
+        .container{
+            width: 1300px;
+            border: 1px solid black;
+        }
+    }   
+    
+    .events{
+        width: 100vw;
+        height: 725px;
+        background-color: lightcyan;
+
+        .container{
+            width: 1300px;
+            border: 1px solid black;
+
+        }
+    }
 
 </style>
