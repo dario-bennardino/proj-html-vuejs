6708cd84737b9f1e60b9f54540f88928b2aa26ae
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-04-09 10:27:00 +0200

    [add] creato colonne header

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -12,9 +12,17 @@ import { store } from '../data/store'
 <template>
 
     <div class="footer">
-        Footer
+        <div class="container">
+            <div class="row row-cols-4 h-100">
+                <div class="col-3 d-flex justify-content-center align-items-center ">col</div>
+                <div class="col-3 d-flex justify-content-center align-items-center">col</div>
+                <div class="col-3 d-flex justify-content-center align-items-center">col</div>
+                <div class="col-3 d-flex justify-content-center align-items-center">col</div>
+            </div>
+        </div>
+        
     </div>
-    
+
 </template>
 
 
@@ -27,5 +35,16 @@ import { store } from '../data/store'
         width: 100vw;
         background-color: $bg-color-8;
         border: 1px solid black;
+
+        .container{
+            width: 1300px;
+            height: 100%;
+            border: 1px solid black;
+
+            .col-3{
+                border: 1px solid black;
+                height: 100%;
+            }
+        }
     }
 </style>
