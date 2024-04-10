309eea6be1288bd1b3882d7dce15fc05d6f1003d
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-04-10 18:27:09 +0200

    [add] jumbotron corretto

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -23,7 +23,7 @@ import { store } from '../data/store'
         <div class="container_ d-flex ">
 
             <div class="box-text-jumbotron">
-                <h1 class="text-start">Devotion that never never <span class="fw-bold">ends</span></h1>
+                <h1 class="text-start">Devotion that never <span class="fw-bold">ends</span></h1>
                 <p>Neque porro quisquam est, qui dolorem ipsum quia doloris sit amet consectetur, adipisci velit, sed quia non numquam eius modi</p>
                 <button type="button" class="btn btn-outline-warning">READ MORE</button>
             </div>
@@ -39,10 +39,22 @@ import { store } from '../data/store'
             </div>
         </div>
         <div class="img-jumbo-first">
-            <img src="/public/assets/img/short-slider-rev-1-img-2.png" alt="">
+            <img src="/public/assets/img/short-slider-rev-1-img-2.png" alt="immagine foglia 1">
         </div>
         <div class="img-jumbo-second">
-            <img src="/public/assets/img/short-slider-rev-1-img-6.png" alt="">
+            <img src="/public/assets/img/short-slider-rev-1-img-6.png" alt="immagine foglia 2">
+        </div>
+        <div class="img-jumbo-third">
+            <img src="/public/assets/img/h-2-slider-img-14.png" alt="immagine foglia 3">
+        </div>
+        <div class="img-jumbo-fourth">
+            <img src="/public/assets/img/h-2-slider-img-17.png" alt="immagine foglia 4">
+        </div>
+        <div class="img-jumbo-fifth">
+            <img src="/public/assets/img/h-2-slider-img-13.png" alt="immagine foglia 5">
+        </div>
+        <div class="img-jumbo-sixth">
+            <img src="/public/assets/img/h-2-slider-img-12.png" alt="immagine foglia 6">
         </div>
     </div>
 </template>
@@ -71,6 +83,30 @@ import { store } from '../data/store'
             right: 295px;
         }
 
+        .img-jumbo-third{
+            position: absolute;
+            top: 80px;
+            right: 365px;
+        }
+
+        .img-jumbo-fourth{
+            position: absolute;
+            bottom: 150px;
+            right: 180px;
+        }
+
+        .img-jumbo-fifth{
+            position: absolute;
+            bottom: 170px;
+            right: 1020px;
+        }
+
+        .img-jumbo-sixth{
+            position: absolute;
+            bottom: 350px;
+            right: 1050px;
+        }
+
         .container_{
 
             width: 1450px;
@@ -80,7 +116,7 @@ import { store } from '../data/store'
                 width: 50%;
                 height: 100%;
                 background-color: white;
-                padding: 50px;
+                padding-right: 140px;
 
                 h1{
                     font-size: 4.5rem;
